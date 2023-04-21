e0daa2dac6fc3fd99a55e1c1b6f41f7c7599ad75
pBouillon <pro.pierre.bouillon@protonmail.com>
2023-04-21 14:19:59 +0200

    Test that everything is correctly provided

---
+++ projects/core/src/provide-ngx-flagr.spec.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { TestBed } from '@angular/core/testing';
+
+import { FeatureFlagDirective } from './feature-flag.directive';
+import { FeatureFlagService } from './feature-flag.service';
+import { provideNgxFlagr } from './provide-ngx-flagr';
+import { FEATURE_FLAG_SERVICE } from './tokens';
+
+@Injectable()
+class TestFeatureFlagService implements FeatureFlagService {
+  isEnabled(): boolean {
+    return true;
+  }
+}
+
+describe(provideNgxFlagr.name, () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        TestFeatureFlagService,
+        provideNgxFlagr({
+          featureFlagService: TestFeatureFlagService,
+        }),
+      ],
+    });
+  });
+
+  it('provides the FeatureFlag directive', () => {
+    expect(() => TestBed.inject(FeatureFlagDirective)).not.toThrowError(
+      /No provider for FeatureFlagDirective/
+    );
+  });
+
+  it('provides the desired service', () => {
+    expect(TestBed.inject(FEATURE_FLAG_SERVICE)).toBeInstanceOf(
+      TestFeatureFlagService
+    );
+  });
+});
