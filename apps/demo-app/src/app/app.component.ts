f6166aed4099adbceb43afe1d1c5e2019a250949
pBouillon <pro.pierre.bouillon@protonmail.com>
2023-07-24 06:28:52 +0200

    Add a dynamic example

--- apps/demo-app/src/app/app.component.ts
+++ apps/demo-app/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { CommonModule, JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 
-import { FeatureFlagDirective } from 'projects/core/src';
+import { FeatureFlagDirective, FEATURE_FLAG_SERVICE } from 'projects/core/src';
 
 import { featureFlagsDefinition } from './app.config';
 import { HeroComponent } from './hero.component';
@@ -15,20 +15,24 @@ import { HeroComponent } from './hero.component';
 
     <hr style="margin: 30px auto" />
 
-    <section>
-      <h1>Configuration</h1>
+    <h1>Configuration</h1>
 
-      <p>
-        Running with the following feature flag definitions:
-        <code>{{ featureFlagsDefinition | json }}</code>
-      </p>
-    </section>
+    <blockquote>
+      Running with the following feature flag definitions:
+      <code>{{ featureFlagsDefinition | json }}</code>
+    </blockquote>
 
     <div class="grid">
+      <!-- Directive only -->
       <div>
         <h3>
           <code>Feature 1</code>
         </h3>
+
+        <blockquote>
+          The <code>featureFlag</code> directive can conditionally render
+          templates
+        </blockquote>
 
         <p *featureFlag="'Feature 1'">
           I'm shown because <code>Feature 1</code> is
@@ -36,10 +40,16 @@ import { HeroComponent } from './hero.component';
         </p>
       </div>
 
+      <!-- With Fallback -->
       <div>
         <h3>
           <code>Feature 2</code>
         </h3>
+
+        <blockquote>
+          A fallback template can be defined if the feature flag is evaluated to
+          false
+        </blockquote>
 
         <p *featureFlag="'Feature 2'; else feature2Disabled">
           <code class="tag valid">Feature 2</code> is
@@ -53,6 +63,36 @@ import { HeroComponent } from './hero.component';
           </p>
         </ng-template>
       </div>
+
+      <!-- From the logic -->
+      <div>
+        <h3>
+          <code>{{ customFeatureFlag() }}</code>
+        </h3>
+
+        <blockquote>
+          Feature flags can be evaluated dynamically from the component's logic
+        </blockquote>
+
+        <input
+          type="text"
+          name="customFeatureFlag"
+          [value]="customFeatureFlag()"
+          (input)="setFeatureFlag($event)"
+        />
+
+        <p>
+          <code class="tag">{{ customFeatureFlag() }}</code> is
+          <span
+            class="tag"
+            [ngClass]="{
+            'valid': customFeatureFlagEvaluation(),
+            'invalid': !customFeatureFlagEvaluation(),
+          }"
+            >{{ customFeatureFlagEvaluation() ? 'enabled' : 'disabled' }}</span
+          >
+        </p>
+      </div>
     </div>
   `,
   styles: [
@@ -62,5 +102,17 @@ import { HeroComponent } from './hero.component';
   ],
 })
 export class AppComponent {
+  readonly #featureFlagService = inject(FEATURE_FLAG_SERVICE);
+
   readonly featureFlagsDefinition = featureFlagsDefinition;
+
+  readonly customFeatureFlag = signal<string>('Feature 3');
+  readonly customFeatureFlagEvaluation = computed(() =>
+    this.#featureFlagService.isEnabled(this.customFeatureFlag())
+  );
+
+  setFeatureFlag(event: any): void {
+    const featureFlag = event.target.value;
+    this.customFeatureFlag.set(featureFlag);
+  }
 }
--- apps/demo-app/src/app/app.config.ts
+++ apps/demo-app/src/app/app.config.ts
@@ -5,6 +5,7 @@ import { provideNgxFlagr, MapBasedFeatureFlagService } from 'projects/core/src';
 export const featureFlagsDefinition: Record<string, boolean> = {
   'Feature 1': true,
   'Feature 2': false,
+  'Feature 3': true,
 };
 
 export const appConfig: ApplicationConfig = {
